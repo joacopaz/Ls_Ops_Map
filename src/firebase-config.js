93ef4e9050ce56ed4ccf684e783a51932b858119
joacopaz <joacopaz@gmail.com>
2022-12-12 12:34:14 -0300

    multi vc handling

--- src/firebase-config.js
+++ src/firebase-config.js
@@ -2,15 +2,18 @@ import firebase from "firebase/compat/app";
 import "firebase/compat/auth";
 import { getFirestore } from "firebase/firestore";
 
-const app = firebase.initializeApp({
-	apiKey: process.env.REACT_APP_API_KEY,
-	authDomain: process.env.REACT_APP_AUTH_DOMAIN,
-	databaseURL: process.env.REACT_APP_DATABASEURL,
-	projectId: process.env.REACT_APP_POROJECTID,
-	storageBucket: process.env.REACT_APP_STORAGEBUCKET,
-	messagingSenderId: process.env.REACT_APP_MESSAGINGSENDERID,
-	appId: process.env.REACT_APP_APPID,
-});
+const app = firebase.initializeApp(
+	{
+		apiKey: process.env.REACT_APP_API_KEY,
+		authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+		databaseURL: process.env.REACT_APP_DATABASEURL,
+		projectId: process.env.REACT_APP_POROJECTID,
+		storageBucket: process.env.REACT_APP_STORAGEBUCKET,
+		messagingSenderId: process.env.REACT_APP_MESSAGINGSENDERID,
+		appId: process.env.REACT_APP_APPID,
+	},
+	{ experimentalAutoDetectLongPolling: true }
+);
 
 export const auth = app.auth();
 export const db = getFirestore(app);
