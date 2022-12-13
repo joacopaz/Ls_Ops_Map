4e1e6ee0bd7dfb20a3929ba54a3967a9ae9b71ec
joacopaz <joacopaz@gmail.com>
2022-12-13 04:01:54 -0300

    fixed JSON storage managing issue (no stringifying version)

--- src/firebase-config.js
+++ src/firebase-config.js
@@ -4,18 +4,16 @@ import firebase from "firebase/compat/app";
 import "firebase/compat/auth";
 import { getFirestore, enableIndexedDbPersistence } from "firebase/firestore";
 
-const app = firebase.initializeApp(
-	{
-		apiKey: process.env.REACT_APP_API_KEY,
-		authDomain: process.env.REACT_APP_AUTH_DOMAIN,
-		databaseURL: process.env.REACT_APP_DATABASEURL,
-		projectId: process.env.REACT_APP_POROJECTID,
-		storageBucket: process.env.REACT_APP_STORAGEBUCKET,
-		messagingSenderId: process.env.REACT_APP_MESSAGINGSENDERID,
-		appId: process.env.REACT_APP_APPID,
-	},
-	{ experimentalForceLongPolling: true }
-);
+const app = firebase.initializeApp({
+	apiKey: process.env.REACT_APP_API_KEY,
+	authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+	databaseURL: process.env.REACT_APP_DATABASEURL,
+	projectId: process.env.REACT_APP_POROJECTID,
+	storageBucket: process.env.REACT_APP_STORAGEBUCKET,
+	messagingSenderId: process.env.REACT_APP_MESSAGINGSENDERID,
+	appId: process.env.REACT_APP_APPID,
+	experimentalForceLongPolling: true,
+});
 
 // initializeAppCheck(app, {
 // 	provider: new ReCaptchaV3Provider(process.env.REACT_APP_RECAPTCHA_KEY),
