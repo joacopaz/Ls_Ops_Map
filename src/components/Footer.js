e5e50ab1eb0bc8e21f5ad5c4e3c7e9a0a1878792
joacopaz <joacopaz@gmail.com>
2023-03-06 10:35:06 -0300

    hidden policies

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,14 +4,14 @@ import styles from "../Mapa.module.css";
 export default function Footer() {
 	return (
 		<>
-			<div className={styles.policy}>
+			{/* <div className={styles.policy}>
 				<a href="../privacy" target="_blank" alt="privacy policy">
 					Privacy Policy
 				</a>
 				<a href="../terms" target="_blank" alt="terms of use">
 					Terms of Use
 				</a>
-			</div>
+			</div> */}
 			<div className={styles.build}>Build 0.4</div>
 		</>
 	);
