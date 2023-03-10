a28efa75c3d7497220a3e3234de8ecbdf9f4202a
joacopaz <joacopaz@gmail.com>
2023-03-09 22:24:30 -0300

    Attempting to fix cors

--- functions/index.js
+++ functions/index.js
@@ -8,7 +8,7 @@ const cors = require("cors");
 const app = express();
 app.use(
 	cors({
-		origin: "*",
+		origin: "https://ls-ops-map.web.app",
 		methods: ["POST", "DELETE"],
 		credentials: true,
 	})
